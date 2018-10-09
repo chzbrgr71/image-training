bd07a4ddd8a97e2b41e09b7ab79074a3acfff9c5
Brian Redmond <brianisrunning@gmail.com>
2018-10-08 19:33:46 -0700

    fixed secrets

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # image-training
+
+helm install --name brig-proj-training brigade/brigade-project -f brig-proj-training.yaml
--- brigade.js
+++ brigade.js
@@ -4,9 +4,10 @@ events.on("push", (brigadeEvent, project) => {
     
     // setup variables
     var acrName = project.secrets.acrName
-    var azServicePrincipal = project.secrets.appId
-    var azClientSecret = project.secrets.password
-    var azTenant = project.secrets.tenant
+    var azServicePrincipal = project.secrets.azServicePrincipal
+    var azClientSecret = project.secrets.azClientSecret
+    var azTenant = project.secrets.azTenant
+    var slackWebhook = project.secrets.slackWebhook
     var gitPayload = JSON.parse(brigadeEvent.payload)
     var gitSHA = brigadeEvent.revision.commit.substr(0,7)
     var branch = getBranch(gitPayload)    
