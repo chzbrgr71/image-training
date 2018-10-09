7e7926c4e803be1a9fb8e3f7978680862d897bf0
Brian Redmond <brianisrunning@gmail.com>
2018-10-09 14:18:26 -0700

    testing

--- brigade.js
+++ brigade.js
@@ -25,8 +25,9 @@ events.on("push", (brigadeEvent, project) => {
     //acr.image = "briaracr.azurecr.io/chzbrgr71/microsoft/azure-cli:2.0.46"
     acr.tasks = [
         `cd /src`,
-        `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
-        `az acr build -t chzbrgr71/image-retrain:${imageTag} -r ${acrName} .`
+        //`az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
+        //`az acr build -t chzbrgr71/image-retrain:${imageTag} -r ${acrName} .`
+        `while true; sleep 1; done`
     ]
 
     // setup brigade job deploying TFJob in Kubernetes
