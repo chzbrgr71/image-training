c3e4abc35fa4e1e57b8695d26b3ba3fc6b62e24c
Brian Redmond <brianisrunning@gmail.com>
2019-01-29 14:42:14 -0500

    new testing

--- brigade.js
+++ brigade.js
@@ -7,29 +7,23 @@ events.on("push", (brigadeEvent, project) => {
     var azServicePrincipal = project.secrets.azServicePrincipal
     var azClientSecret = project.secrets.azClientSecret
     var azTenant = project.secrets.azTenant
-    var slackWebhook = project.secrets.slackWebhook
     var gitPayload = JSON.parse(brigadeEvent.payload)
     var gitSHA = brigadeEvent.revision.commit.substr(0,7)
     var branch = getBranch(gitPayload)    
     var imageTag = branch + "-" + gitSHA
 
     console.log(`==> gitHub webook on ${branch} branch with commit ID ${gitSHA}`)
-    console.log(`servprin=${azServicePrincipal}`)
-    console.log(`secret=${azClientSecret}`)
-    console.log(`tenant=${azTenant}`)
 
     // setup brigade job to build container images
-    var acr = new Job("job-runner-acr-builder")
+    var acr = new Job("job-runner-acr-task")
     acr.storage.enabled = false
-    acr.image = "microsoft/azure-cli:2.0.46"
-    //acr.image = "briaracr.azurecr.io/chzbrgr71/microsoft/azure-cli:2.0.46"
+    acr.image = "microsoft/azure-cli:2.0.55"
     acr.tasks = [
         `cd /src`,
         `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
         `az acr build -t chzbrgr71/image-retrain:${imageTag} -r ${acrName} .`
-        //`while true; do echo "Brian"; sleep 1; done`
     ]
-
+    /*
     // setup brigade job deploying TFJob in Kubernetes
     var helm = new Job("job-runner-helm")
     helm.storage.enabled = false
@@ -37,11 +31,12 @@ events.on("push", (brigadeEvent, project) => {
     helm.tasks = [
         `helm install --name image-retrain --set imageTag=${imageTag} ./chart`
     ]
-
+    */
+   
     // create a brigade group and run
     var pipeline = new Group()
     pipeline.add(acr)
-    pipeline.add(helm)
+    //pipeline.add(helm)
     pipeline.runEach()
 
 })
