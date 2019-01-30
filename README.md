1f6bc58518eade731bed23cdf4fdef80d88d8b03
Brian Redmond <brianisrunning@gmail.com>
2019-01-30 12:51:19 -0500

    added helm portion

--- README.md
+++ README.md
@@ -40,3 +40,52 @@ PODNAME=
 kubectl cp default/$PODNAME:/tmp/tensorflow/tf-output/retrained_graph.pb ~/Downloads/retrained_graph.pb
 kubectl cp default/$PODNAME:/tmp/tensorflow/tf-output/retrained_labels.txt ~/Downloads/retrained_labels.txt
 ```
+
+* Local machine
+
+```bash
+docker build -t chzbrgr71/tf-training .
+
+docker run -d --name tf \
+--volume /Users/brianredmond/gopath/src/github.com/chzbrgr71/image-training/output:/output \
+chzbrgr71/tf-training \
+--bottleneck_dir=bottlenecks \
+--model_dir=/tmp/tensorflow/tf-output/inception \
+--summaries_dir=/output/training_summaries/baseline \
+--output_graph=/output/retrained_graph.pb \
+--output_labels=/output/retrained_labels.txt \
+--image_dir=images \
+--learning_rate=0.01 \
+--train_batch_size=100
+
+# tensorboard
+/usr/local/bin/tensorboard --logdir /tmp/tensorflow/tf-output/training_summaries/baseline
+
+ docker run -d --name tensorboard \
+ -p 6006:6006 \
+ --volume /Users/brianredmond/gopath/src/github.com/chzbrgr71/image-training/output:/output \
+ chzbrgr71/tensorboard:1.9 \
+ --logdir=/output/training_summaries
+```
+
+### Testing without Kubeflow
+
+* Setup storage
+
+```bash
+export RG_NAME=MC_briar-aks-east-100_briar-aks-east-100_eastus
+export STORAGE=briartftraining
+
+az storage account create --resource-group $RG_NAME --name $STORAGE --sku Standard_LRS
+```
+
+```bash
+kubectl create -f ./k8s/sc.yaml
+kubectl create -f ./k8s/pvc.yaml
+```
+
+* Deploy
+
+```bash
+helm install --name tf-training ./chart-deploy
+```
--- brigade.js
+++ brigade.js
@@ -9,8 +9,10 @@ events.on("push", (brigadeEvent, project) => {
     var azTenant = project.secrets.azTenant
     var gitPayload = JSON.parse(brigadeEvent.payload)
     var gitSHA = brigadeEvent.revision.commit.substr(0,7)
-    var branch = getBranch(gitPayload)    
-    var imageTag = branch + "-" + gitSHA
+    var branch = getBranch(gitPayload)
+    var image = "chzbrgr71/image-retrain"
+    var imageTag = gitSHA
+    var jobIdentifier = gitSHA
 
     console.log(`==> gitHub webook on ${branch} branch with commit ID ${gitSHA}`)
 
@@ -21,22 +23,21 @@ events.on("push", (brigadeEvent, project) => {
     acr.tasks = [
         `cd /src`,
         `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
-        `az acr build -t chzbrgr71/image-retrain:${imageTag} -r ${acrName} .`
+        `az acr build -t ${image}:${imageTag} -r ${acrName} .`
     ]
-    /*
-    // setup brigade job deploying TFJob in Kubernetes
+    
+    // setup brigade job to run training job in kubernetes
     var helm = new Job("job-runner-helm")
     helm.storage.enabled = false
-    helm.image = "briaracr.azurecr.io/chzbrgr71/k8s-helm:v2.10.0"
+    helm.image = "lachlanevenson/k8s-helm:v2.12.3"
     helm.tasks = [
-        `helm install --name image-retrain --set imageTag=${imageTag} ./chart`
+        `helm install --name tf-training --set name=${jobIdentifier},container.image=${image},container.imageTag=${imageTag} ./chart-deploy`
     ]
-    */
    
     // create a brigade group and run
     var pipeline = new Group()
     pipeline.add(acr)
-    //pipeline.add(helm)
+    pipeline.add(helm)
     pipeline.runEach()
 
 })
